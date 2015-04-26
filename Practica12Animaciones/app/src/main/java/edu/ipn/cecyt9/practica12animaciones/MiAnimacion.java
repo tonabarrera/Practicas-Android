322bc6bd01975bc34ea0f1c0bd05dbb99bbd4497
Carlos Tonatihu <carlostonatihu@gmail.com>
2015-04-25 22:46:34 -0500

    Faltan comentarios de las ultimas 2 practicas

--- Practica12Animaciones/app/src/main/java/edu/ipn/cecyt9/practica12animaciones/MiAnimacion.java
+++ Practica12Animaciones/app/src/main/java/edu/ipn/cecyt9/practica12animaciones/MiAnimacion.java
@@ -1,15 +1,15 @@
 package edu.ipn.cecyt9.practica12animaciones;
 
-import android.support.v7.app.ActionBarActivity;
+import android.animation.AnimatorInflater;
+import android.animation.AnimatorSet;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
+import android.view.View;
 import android.widget.TextView;
 
 
-public class MiAnimacion extends ActionBarActivity {
+public class MiAnimacion extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,8 +17,12 @@ public class MiAnimacion extends ActionBarActivity {
         setContentView(R.layout.activity_mi_animacion);
 
         TextView texto = (TextView) findViewById(R.id.text_titulo);
-        Animation animacion = AnimationUtils.loadAnimation(this, R.anim.animacion);
-        texto.startAnimation(animacion);
-
+        AnimatorSet set = (AnimatorSet) AnimatorInflater.loadAnimator(this, R.animator.mi_animacion);
+        set.setTarget(texto);
+        set.start();
+    }
+    public void onClickMainActivity(View view){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
