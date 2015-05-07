053d2fe6a4c522500132ba55aa34f3cc33d6430f
Carlos Tonatihu <carlostonatihu@gmail.com>
2015-05-07 16:27:36 -0500

    Agregando todo lo necesario

--- Practica14Hilos/app/src/main/java/edu/ipn/cecyt9/practica14hilos/MainActivity.java
+++ Practica14Hilos/app/src/main/java/edu/ipn/cecyt9/practica14hilos/MainActivity.java
@@ -1,38 +1,235 @@
 package edu.ipn.cecyt9.practica14hilos;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.os.SystemClock;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
+    private EditText entrada;
+    private TextView salida;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        entrada = (EditText) findViewById(R.id.entrada);
+        salida = (TextView) findViewById(R.id.salida);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    public void calcularOperacion(View view) {
+        int n = Integer.parseInt(entrada.getText().toString());
+        salida.append(n +"! = ");
+        int res = factorial(n);
+        salida.append(res + "\n");
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    public int factorial(int n) {
+        int res=1;
+        for (int i=1; i<=n; i++){
+            res*=i;
+            SystemClock.sleep(1000);
         }
 
-        return super.onOptionsItemSelected(item);
+        return res;
+
     }
+
+    class MiThread extends Thread {
+        private int n, res;
+
+        public MiThread(int n) {
+            this.n = n;
+        }
+
+        @Override
+        public void run() {
+            res = factorial(n);
+            salida.append(res + "\n");
+        }
+
+//        @Override
+//        public void run() {
+//            res = factorial(n);
+//            runOnUiThread(new Runnable() {
+//                @Override
+//                public void run() {
+//                    salida.append(res + "\n");
+//                }
+//            });
+//        }
+    }
+
+//    public void calcularOperacion(View view) {
+//        int n = Integer.parseInt(entrada.getText().toString());
+//        salida.append(n + "! = ");
+//        MiThread thread = new MiThread(n);
+//        thread.start();
+//    }
+
+
+//    ejemplo AsyncTask
+//    class MiTarea extends AsyncTask<Integer, Void, Integer> {
+//
+//        @Override
+//
+//        protected Integer doInBackground(Integer... n) {
+//
+//            return factorial(n[0]);
+//
+//        }
+//
+//        @Override
+//
+//        protected void onPostExecute(Integer res) {
+//
+//            salida.append(res + "\n");
+//
+//        }
+//
+//    }
+
+//        public void calcularOperacion(View view) {
+//        int n = Integer.parseInt(entrada.getText().toString());
+//        salida.append(n + "! = ");
+//        MiTarea tarea = new MiTarea();
+//        tarea.execute(n);
+//
+//        }
+
+    //    ejemplo AsyncTask whit progressdialog
+//    class MiTarea extends AsyncTask<Integer, Integer, Integer> {
+//
+//        private ProgressDialog progreso;
+//
+//        @Override protected void onPreExecute() {
+//
+//            progreso = new ProgressDialog(MainActivity.this);
+//
+//            progreso.setProgressStyle(ProgressDialog.
+//                    STYLE_HORIZONTAL);
+//
+//            progreso.setMessage("Calculando...");
+//
+//            progreso.setCancelable(false);
+//
+//            progreso.setMax(100);
+//
+//            progreso.setProgress(0);
+//
+//            progreso.show();
+//
+//        }
+//
+//        @Override protected Integer doInBackground(Integer... n) {
+//
+//            int res = 1;
+//
+//            for (int i = 1; i <= n[0]; i++) {
+//
+//                res *= i;
+//
+//                SystemClock.sleep(1000);
+//
+//                publishProgress(i*100 / n[0]);
+//
+//            }
+//
+//            return res;
+//
+//        }
+//
+//        @Override protected void onProgressUpdate(Integer... porc) {
+//
+//            progreso.setProgress(porc[0]);
+//
+//        }
+//
+//        @Override protected void onPostExecute(Integer res) {
+//
+//            progreso.dismiss();
+//
+//            salida.append(res + "\n");
+//
+//        }
+//
+//    }
+
+    //    ejemplo AsyncTask whit progressdialog cancel
+//    class MiTarea extends AsyncTask<Integer, Integer, Integer> {
+//
+//        private ProgressDialog progreso;
+//
+//        @Override protected void onPreExecute() {
+//
+//            progreso = new ProgressDialog(MainActivity.this);
+//
+//            progreso.setProgressStyle(ProgressDialog.
+//                    STYLE_HORIZONTAL);
+//
+//            progreso.setMessage("Calculando...");
+//
+//            progreso.setCancelable(true);
+//
+//            progreso.setOnCancelListener(new OnCancelListener() {
+//
+//                @Override
+//                public void onCancel(DialogInterface dialog) {
+//
+//                    MiTarea.this.cancel(true);
+//
+//                }
+//
+//            });
+//
+//            progreso.setMax(100);
+//
+//            progreso.setProgress(0);
+//
+//            progreso.show();
+//
+//        }
+//
+//        @Override protected Integer doInBackground(Integer... n) {
+//
+//            int res = 1;
+//
+//            for (int i = 1; i <= n[0] && !isCancelled(); i++) {
+//                res *= i;
+//
+//                SystemClock.sleep(1000);
+//
+//                publishProgress(i*100 / n[0]);
+//
+//            }
+//
+//            return res;
+//
+//        }
+//
+//        @Override protected void onProgressUpdate(Integer... porc) {
+//
+//            progreso.setProgress(porc[0]);
+//
+//        }
+//
+//        @Override protected void onPostExecute(Integer res) {
+//
+//            progreso.dismiss();
+//
+//            salida.append(res + "\n");
+//
+//        }
+//
+//        @Override protected void onCancelled() {
+//
+//            salida.append("cancelado\n");
+//
+//        }
+//
+//    }
+
 }
