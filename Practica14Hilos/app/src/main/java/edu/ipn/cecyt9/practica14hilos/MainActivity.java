9b474591b8b3b0c1fb83457634c186c621828e04
Carlos Tonatihu <carlostonatihu@gmail.com>
2015-05-08 18:15:23 -0500

    Ya estan los comentarios

--- Practica14Hilos/app/src/main/java/edu/ipn/cecyt9/practica14hilos/MainActivity.java
+++ Practica14Hilos/app/src/main/java/edu/ipn/cecyt9/practica14hilos/MainActivity.java
@@ -1,5 +1,20 @@
-package edu.ipn.cecyt9.practica14hilos;
+/*
+AUTOR: Barrera Pérez Carlos Tonatihu
 
+VERSION: 2.0
+
+DESCRIPCION: Este programa junta la practica 14 en la que se usan hilos, especificanmente la clase AsyncTask
+
+OBSERVACIONES: En la primera version solo se calculaba el factorial de un numero, en esta version se calcula un numero en la serie de fibonacci en base a su posicion
+
+COMPILACION: Se compila cuando se ejecuta
+
+EJECUCION: Solo se da click en el boton 'run app', shift + F10 o click en la pestaña 'Run' y después
+        click en 'Run App' y se selecciona el emulador o un dispositivo android.
+*/
+
+package edu.ipn.cecyt9.practica14hilos;
+//importamos las librerias necesarias
 import android.app.Activity;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -9,6 +24,7 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
+    //Declaramos los elementos que vamos a usar 
     private EditText entrada;
     private TextView salidaFactorial;
     private TextView salidaSerie;
@@ -17,19 +33,20 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        //inicializamos nuestros elementos
         entrada = (EditText) findViewById(R.id.entrada);
         salidaFactorial = (TextView) findViewById(R.id.salida_factorial);
         salidaSerie = (TextView) findViewById(R.id.salida_serie);
     }
-
+    //Este metodo se ejecuta cuando se pulsa el boton de calcular
     public void calcularOperacion(View view) {
-        int n = Integer.parseInt(entrada.getText().toString());
+        int n = Integer.parseInt(entrada.getText().toString());//Recuperamos el num a ser calculado
         salidaFactorial.append("\n" + n +"! = ");
         salidaSerie.append("\n" + n +" = ");
-
+        //Creamos nuestros objetos
         MiTarea  tarea = new MiTarea();
         SerieHilo serieHilo = new SerieHilo();
+        //Ejecutamos nuestros hilos y les pasamos el numero con el que se va a calcular el factorial y el numero de la serie
         serieHilo.execute(n);
         tarea.execute(n);
     }
@@ -52,23 +69,24 @@ public class MainActivity extends Activity {
             salidaFactorial.append(String.valueOf(res));
         }
     }
-
+    //Creamos una clase que herede de AsyncTask y que va a utilizar datos de tipo Integer
     class SerieHilo extends AsyncTask<Integer, Void, Integer> {
-
+        //Sobrecargamos nuestro metodo y en este recibe el numero que le pasamos
         @Override
         protected Integer doInBackground(Integer... params) {
             int var = 1;
             int resultado = 0;
+            //con este loop calculamos el resultado de la serie se fibonacci
             for(int i = 0; i < params[0]; i++){
                 resultado = resultado + var;
                 var = resultado - var;
             }
-            return resultado;
+            return resultado; //Retornamos el resultado de tipo Integer
         }
-
+        //Sobrecargamos y aqui se recibe el valor que se retorna y que es del mismo tipo
         @Override
         protected void onPostExecute(Integer resultado) {
-            salidaSerie.append(String.valueOf(resultado));
+            salidaSerie.append(String.valueOf(resultado)); //Solo añadimos el resultado al TextView
         }
     }
 }
--- Practica16Eventos/app/src/main/java/edu/ipn/cecyt9/practica16eventos/MainActivity.java
+++ Practica16Eventos/app/src/main/java/edu/ipn/cecyt9/practica16eventos/MainActivity.java
@@ -1,5 +1,20 @@
-package edu.ipn.cecyt9.practica16eventos;
+/*
+AUTOR: Barrera Pérez Carlos Tonatihu
 
+VERSION: 2.0
+
+DESCRIPCION: Este programa junta la practica 16 en la que se dibujan circulos con los dedos
+
+OBSERVACIONES: La primera version se dibujaba una linea que seguia nuestro dedo, en esta version esa linea es rempladaza con un circulo
+
+COMPILACION: Se compila cuando se ejecuta
+
+EJECUCION: Solo se da click en el boton 'run app', shift + F10 o click en la pestaña 'Run' y después
+        click en 'Run App' y se selecciona el emulador o un dispositivo android.
+*/
+
+package edu.ipn.cecyt9.practica16eventos;
+//importamos lo necesario
 import android.app.Activity;
 import android.content.Context;
 import android.graphics.Canvas;
@@ -7,7 +22,6 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 
@@ -18,19 +32,20 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        SpecialView miVista = new SpecialView(this);
-        setContentView(miVista);
+        SpecialView miVista = new SpecialView(this);//Cremos nuestra vista perzonalizada pasandole
+        setContentView(miVista); //Inflamos nuestra vista
     }
-
+    //Esta sera nuestra vista especial, una clase que hereda de View
     class SpecialView extends View {
         float x = 50;
-        float radio = 50;
+        float radio = 50; //Radio del circulo
         float y = 50;
         String accion = "Accion";
         String texto = "Evento";
+        //Delaramos nuestro path y paint
         Path path;
         Paint paint;
-
+        //Nuestro constructor
         public SpecialView(Context context) {
             super(context);
             path = new Path();
@@ -39,18 +54,19 @@ public class MainActivity extends Activity {
 
         @Override
         protected void onDraw(Canvas canvas) {
-            Log.e("Hola", "hola");
              // color de fondo
             paint.setStyle(Paint.Style.STROKE);
             paint.setStrokeWidth(6);
             paint.setColor(Color.RED);
-
+            //Se ejecuta cuando se pulsa la pantalla y dibuja un circulo
             if (accion.equals("down")) {
                 path.addCircle(x, y, radio, Path.Direction.CCW);
             }
+            //Se ejecuta cuando se mueve el dedo sobre la pantalla y dibuja un circulo
             if (accion.equals("move")) {
                 path.addCircle(x, y, radio, Path.Direction.CCW);
             }
+            //le pasamos nuestro trazo al canvas para que este lo dibuje
             canvas.drawPath(path, paint);
             paint.setColor(Color.BLACK);
             paint.setTextSize(20);
@@ -58,19 +74,22 @@ public class MainActivity extends Activity {
             canvas.drawText(texto, 100, 130, paint);
             canvas.drawText("x = " + x + "  y = " + y, 100, 50, paint);
         }
-
+        //Con este metodo detectamos las acciones sobre la pantalla
         @Override
         public boolean onTouchEvent(MotionEvent evento) {
+            //obtemeos las cordenadas
             x = evento.getX();
             y = evento.getY();
-
+            //Es verdadera si se pulsa la pantalla
             if (evento.getAction() == MotionEvent.ACTION_DOWN) {
                 accion = "down";
                 texto = "Action Down";
             }
+            //Es verdadera si se levanta el dedo de la pantalla
             if (evento.getAction() == MotionEvent.ACTION_UP) {
                 texto = "Action Up";
             }
+            //Es verdadera si se mueve el dedo sobre la pantalla
             if (evento.getAction() == MotionEvent.ACTION_MOVE) {
                 accion = "move";
                 texto = "Action Move";
