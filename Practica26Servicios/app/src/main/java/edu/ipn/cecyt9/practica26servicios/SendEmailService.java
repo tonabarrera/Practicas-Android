880825080d96ceb52c1043a471d0ad4723bd7661
Carlos Tonatihu <carlostonatihu@gmail.com>
2015-05-30 15:00:49 -0500

    Agregando archivos

---
+++ Practica26Servicios/app/src/main/java/edu/ipn/cecyt9/practica26servicios/SendEmailService.java
@@ -0,0 +1,16 @@
+package edu.ipn.cecyt9.practica26servicios;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class SendEmailService extends Service {
+    public SendEmailService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
